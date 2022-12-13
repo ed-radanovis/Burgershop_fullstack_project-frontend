e32118b73a766c6857b78f1552080caafd2e07d5
Edmar Radanovis <edradanovis@gmail.com>
2022-12-13 14:06:24 -0300

    add url api test

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
+// const apiCodeBurger = axios.create({
+//     baseURL: 'http://localhost:3010'
+// })
+
 const apiCodeBurger = axios.create({
-    baseURL: 'http://localhost:3010'
+    baseURL: 'https://e-commerce-burgershop-api.onrender.com'
 })
+
 
 apiCodeBurger.interceptors.request.use(async config => {
     const userData = await localStorage.getItem('codebuger:userData')
